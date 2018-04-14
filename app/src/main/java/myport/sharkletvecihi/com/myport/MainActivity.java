04376798c2e3262ee9c47d56daf11c8fb9f7203a
esefberfu <esef.berfu@hotmail.com>
2018-04-14 11:06:31 +0300

    full

--- app/src/main/java/myport/sharkletvecihi/com/myport/MainActivity.java
+++ app/src/main/java/myport/sharkletvecihi/com/myport/MainActivity.java
@@ -1,5 +1,6 @@
 package myport.sharkletvecihi.com.myport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //aksldajksdlkajsdlakjdklajd
+
+        Intent intent = new Intent(this, MainBottomActivity.class);
+        startActivity(intent);
     }
 }
