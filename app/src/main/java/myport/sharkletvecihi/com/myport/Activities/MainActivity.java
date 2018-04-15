713ff701e970a4e1e5ab134ddd6156df85a11a22
anlgrses <anilgurses98@gmail.com>
2018-04-15 10:18:15 +0300

    Last Changes

--- app/src/main/java/myport/sharkletvecihi/com/myport/Activities/MainActivity.java
+++ app/src/main/java/myport/sharkletvecihi/com/myport/Activities/MainActivity.java
@@ -1,8 +1,11 @@
 package myport.sharkletvecihi.com.myport.Activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,16 +22,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        int defaultValue = 0;
+        int highScore = sharedPref.getInt("init", defaultValue);
+        Log.v("resp",Integer.toString(highScore));
+
+        if(highScore == 0){
+            Intent intent = new Intent(getApplicationContext(),IntroActivity.class);
+            startActivity(intent);
+        }
+
+
+
         addbtn = (Button)findViewById(R.id.addbtn);
         eatBtn = (Button)findViewById(R.id.eatbtn);
 
         addbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(v.getContext(), fragmentActivity.class);
                 startActivity(intent);
-
             }
         });
 
--- app/src/main/java/myport/sharkletvecihi/com/myport/Activities/toDo.java
+++ app/src/main/java/myport/sharkletvecihi/com/myport/Activities/toDo.java
@@ -1,29 +1,14 @@
 package myport.sharkletvecihi.com.myport.Activities;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import myport.sharkletvecihi.com.myport.Models.rest;
-import myport.sharkletvecihi.com.myport.Fragments.addflight;
-import myport.sharkletvecihi.com.myport.Models.flight;
 import myport.sharkletvecihi.com.myport.R;
-import myport.sharkletvecihi.com.myport.Adapters.restadapter;
+
 
 public class toDo extends AppCompatActivity {
 
@@ -34,6 +19,9 @@ public class toDo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do);
+
+
+
 
 
     }
